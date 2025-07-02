b64be1a2fa491448663fd59bd4898df2020e3f00
ilotod <openfund365@gmail.com>
2025-07-03 07:13:35 +0900

    init
    deploy new contract on arbitrum

---
+++ src/utils/constants.ts
@@ -0,0 +1,45 @@
+import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
+
+// arbitrum-one
+export const STELE_ADDRESS = '0xFBeFb660a91584afB282e2aE8A689765c66a8Fb3'
+export const UNISWAP_V3_FACTORY = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
+export const WETH = '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
+export const USDC = '0xaf88d065e77c8cC2239327C5EDb3A432268e5831'
+
+// Challenge type definition
+export enum ChallengeType { OneWeek, OneMonth, ThreeMonths, SixMonths, OneYear }
+// Duration in seconds for each challenge type
+export function getDuration(challengeType: ChallengeType): BigInt {
+  if (challengeType == ChallengeType.OneWeek) return BigInt.fromI32(7 * 24 * 60 * 60);
+  if (challengeType == ChallengeType.OneMonth) return BigInt.fromI32(30 * 24 * 60 * 60);
+  if (challengeType == ChallengeType.ThreeMonths) return BigInt.fromI32(90 * 24 * 60 * 60);
+  if (challengeType == ChallengeType.SixMonths) return BigInt.fromI32(180 * 24 * 60 * 60);
+  if (challengeType == ChallengeType.OneYear) return BigInt.fromI32(365 * 24 * 60 * 60);
+  return BigInt.fromI32(0);
+}
+
+export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+export const DECIMAL_18 = '1000000000000000000'
+export const DECIMAL_6 = '1000000'
+export const PRICE_CACHE_DURATION = 900 // 15 minutes in seconds
+export const TOKEN_INFO_CACHE_DURATION = 604800 // 1 week in seconds
+export const POOL_INFO_CACHE_DURATION = 604800 // 1 week in seconds for pool metadata
+export const POOL_LIQUIDITY_CACHE_DURATION = 21600 // 6 hours in seconds for pool liquidity
+export const POOL_SLOT0_CACHE_DURATION = 900 // 15 minutes in seconds for pool slot0
+
+export const UNKNWON = 'Unknown'
+
+export const TYPE_NORMAL = 0
+export const TYPE_DEPOSIT = 1
+export const TYPE_WITHDRAW = 2
+
+export let ZERO_BI = BigInt.fromI32(0)
+export let ONE_BI = BigInt.fromI32(1)
+export let ZERO_BD = BigDecimal.fromString('0')
+export let ONE_BD = BigDecimal.fromString('1')
+export let BI_18 = BigInt.fromI32(18)
+
+export const WETH_DECIMAL = BigDecimal.fromString(DECIMAL_18)
+export const USDC_DECIMAL = BigDecimal.fromString(DECIMAL_6)
+
+// export let settingContract = SteleContract.bind(Address.fromString(STELE_ADDRESS))
---
+++ src/utils/snapshots.ts
@@ -0,0 +1,118 @@
+import { Address, BigDecimal, BigInt, Bytes, ethereum, log } from '@graphprotocol/graph-ts'
+import {
+  Stele,
+  Challenge,
+  ChallengeSnapshot,
+  ActiveChallengesSnapshot,
+  Investor,
+  InvestorSnapshot,
+  ActiveChallenges
+} from '../../generated/schema'
+import { STELE_ADDRESS } from './constants'
+import { getInvestorID } from './investor'
+
+export function challengeSnapshot(
+  challengeId: string,
+  event: ethereum.Event
+): void {
+  let challenge = Challenge.load(challengeId)
+  if (!challenge) return
+
+  let timestamp = event.block.timestamp.toI32()
+  let dayID = timestamp / 86400 // rounded
+
+  let challengeSnapshot = ChallengeSnapshot.load(challengeId + "-" + dayID.toString())
+  if (challengeSnapshot == null) {
+    challengeSnapshot = new ChallengeSnapshot(challengeId + "-" + dayID.toString())
+  }
+  challengeSnapshot.challengeId = challenge.challengeId
+  challengeSnapshot.timestamp = event.block.timestamp
+  challengeSnapshot.investorCount = challenge.investorCounter
+  challengeSnapshot.rewardAmountUSD = challenge.rewardAmountUSD
+  challengeSnapshot.topUsers = challenge.topUsers
+  challengeSnapshot.score = challenge.score
+  challengeSnapshot.save()
+}
+
+export function activeChallengesSnapshot(event: ethereum.Event): void {
+  let activeChallenges = ActiveChallenges.load(Bytes.fromI32(0))
+  if (activeChallenges == null) {
+    return
+  }
+
+  let timestamp = event.block.timestamp.toI32()
+  let dayID = timestamp / 86400 // rounded
+
+  let activeChallengesSnapshot = ActiveChallengesSnapshot.load(dayID.toString())
+  if (activeChallengesSnapshot == null) {
+    activeChallengesSnapshot = new ActiveChallengesSnapshot(dayID.toString())
+  }
+  activeChallengesSnapshot.timestamp = event.block.timestamp
+
+  let one_week_investorCounter = activeChallenges.one_week_investorCounter
+  let one_week_rewardAmountUSD = activeChallenges.one_week_rewardAmountUSD
+  let one_month_investorCounter = activeChallenges.one_month_investorCounter
+  let one_month_rewardAmountUSD = activeChallenges.one_month_rewardAmountUSD
+  let three_month_investorCounter = activeChallenges.three_month_investorCounter
+  let three_month_rewardAmountUSD = activeChallenges.three_month_rewardAmountUSD
+  let six_month_investorCounter = activeChallenges.six_month_investorCounter
+  let six_month_rewardAmountUSD = activeChallenges.six_month_rewardAmountUSD
+  let one_year_investorCounter = activeChallenges.one_year_investorCounter
+  let one_year_rewardAmountUSD = activeChallenges.one_year_rewardAmountUSD
+
+  activeChallengesSnapshot.totalParticipants = 
+    one_week_investorCounter
+      .plus(one_month_investorCounter)
+      .plus(three_month_investorCounter)
+      .plus(six_month_investorCounter)
+      .plus(one_year_investorCounter)
+  activeChallengesSnapshot.totalRewards = 
+    one_week_rewardAmountUSD
+      .plus(one_month_rewardAmountUSD)
+      .plus(three_month_rewardAmountUSD)
+      .plus(six_month_rewardAmountUSD)
+      .plus(one_year_rewardAmountUSD)
+  
+  activeChallengesSnapshot.one_week_investorCounter = one_week_investorCounter
+  activeChallengesSnapshot.one_week_rewardAmountUSD = one_week_rewardAmountUSD
+  activeChallengesSnapshot.one_month_investorCounter = one_month_investorCounter
+  activeChallengesSnapshot.one_month_rewardAmountUSD = one_month_rewardAmountUSD
+  activeChallengesSnapshot.three_month_investorCounter = three_month_investorCounter
+  activeChallengesSnapshot.three_month_rewardAmountUSD = three_month_rewardAmountUSD
+  activeChallengesSnapshot.six_month_investorCounter = six_month_investorCounter
+  activeChallengesSnapshot.six_month_rewardAmountUSD = six_month_rewardAmountUSD
+  activeChallengesSnapshot.one_year_investorCounter = one_year_investorCounter
+  activeChallengesSnapshot.one_year_rewardAmountUSD = one_year_rewardAmountUSD
+  activeChallengesSnapshot.save()
+}
+
+export function investorSnapshot(
+  challengeId: BigInt,
+  investorAddress: Bytes,
+  event: ethereum.Event
+): void {
+  const investorId = getInvestorID(
+    challengeId, 
+    Address.fromString(investorAddress.toHexString()))
+  let investor = Investor.load(investorId)
+  if (!investor) return 
+
+  let dayID = event.block.timestamp.toI32() / 86400 // rounded
+
+  // Always create new snapshot for each event
+  let investorSnapshot = InvestorSnapshot.load(investorId + "-" + dayID.toString())
+  if (investorSnapshot == null) {
+    investorSnapshot = new InvestorSnapshot(investorId + "-" + dayID.toString())
+  }
+  investorSnapshot.challengeId = investor.challengeId
+  investorSnapshot.timestamp = event.block.timestamp
+  investorSnapshot.investor = investor.investor
+  investorSnapshot.seedMoneyUSD = investor.seedMoneyUSD
+  investorSnapshot.currentUSD = investor.currentUSD
+  investorSnapshot.tokens = investor.tokens
+  investorSnapshot.tokensAmount = investor.tokensAmount
+  investorSnapshot.tokensDecimals = investor.tokensDecimals
+  investorSnapshot.tokensSymbols = investor.tokensSymbols
+  investorSnapshot.profitRatio = investor.profitRatio
+  investorSnapshot.save()
+}
